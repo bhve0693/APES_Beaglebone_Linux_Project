2e259c9909c94139dbd59d1c95b5df43736fad1b
disa6302 <disa6302@colorado.edu>
2017-10-28 17:15:01 -0600

    message.h header file design stub

---
+++ inc/message.h
@@ -0,0 +1,65 @@
+/*
+* FileName        :    message.h
+* Description     :    This file contains the necessary SW support for message queue, logpackets and link layer functions
+*                        
+* File Author Name:    Divya Sampath Kumar, Bhallaji Venkatesan
+* Tools used      :    gcc, gedit, Sublime Text
+* References      :    
+*
+*
+*/
+
+
+#ifndef _MESSAGE_H
+#define _MESSAGE_H
+
+#include <time.h>
+
+//Level of logs for criticality demarcation in the logfile
+typedef enum loglevel_t{
+LOG_DATA_VALID;     
+LOG_DATA_REQ;
+LOG_DATA_CONV;
+LOG_CRITICAL;
+LOG_INIT;
+LOG_ERR;
+}loglevel;
+
+//Source Task 
+typedef enum srcid_t{
+SRC_MAIN;
+SRC_TEMPERATURE;
+SRC_LIGHT;
+SRC_LOG;
+SRC_DEFAULT;
+}srcid;
+
+//Log Packet
+typedef struct log_packet_t{
+struct timeval *time;  //Timestamps use time.h
+loglevel level;        //Log Levels based on criticality of logs
+srcid sourceid;         //Source of Logs 
+void *logmsg;          //Message to be logged: void * due to flexibile data type requirment
+int32_t crc;           //CRC check for error checking mechanism   
+}logpacket;
+
+
+/* Link Layer Functions */
+int8_t link_req_hb_notif(mq_t msgdt); // Requests Notifications from all the tasks in order of 
+									  // 1.app_tempsensor_task
+					                  // 2.app_lightsensor_task
+					                  // 3.app_sync_logger
+					 
+int8_t link_send_hb_notif(mq_t msgdt); 
+
+int8_t link_send_log_init_status(mq_t msgdt,struct logpacket);
+
+int8_t link_send_log_data_request(mq_t msgdt, struct logpacket);
+
+int8_t link_send_log_err(mq_t msgdt, struct logpacket);
+
+int8_t link_read_data(mq_t msgdt,void* data, srcid source, srcid dest); // Will be called by query functions. Will share the message queues with the sensor task.
+
+int8_t link_send_logs(struct logpacket);
+
+#endif
